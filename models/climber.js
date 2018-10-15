8b1ddef057ffa31d5a300e923e9be58ad3e0e4b1
moskowitza <moskowitzaaron@mac.com>
2018-10-15 13:27:21 -0400

    still working on climber registration

--- models/climber.js
+++ models/climber.js
@@ -1,7 +1,10 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var passportLocalMongoose = require('passport-local-mongoose');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
 var moment = require('moment'); // For date handling.
+var passportLocalMongoose = require('passport-local-mongoose');
+
 
 var ClimberSchema = new Schema({
     username: {type: String, required: true, max: 100},
@@ -24,8 +27,37 @@ ClimberSchema
 .get(function () {
   return '/catalog/climber/'+this._id;
 });
+ClimberSchema.methods.setPassword = function (password) {
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+};
 
+ClimberSchema.methods.validatePassword = function (password) {
+  const hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+  return this.hash === hash;
+};
+
+ClimberSchema.methods.generateJWT = function () {
+  const today = new Date();
+  const expirationDate = new Date(today);
+  expirationDate.setDate(today.getDate() + 60);
+
+  return jwt.sign({
+      email: this.email,
+      id: this._id,
+      exp: parseInt(expirationDate.getTime() / 1000, 10),
+  }, 'secret');
+}
+
+ClimberSchema.methods.toAuthJSON = function () {
+  return {
+      _id: this._id,
+      email: this.email,
+      token: this.generateJWT(),
+  };
+};
 
 ClimberSchema.plugin(passportLocalMongoose);
+
 
 module.exports = mongoose.model('Climber', ClimberSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,48 @@
+// This is from our tutorial as index.js
+//we already have an index route that taks us to the catalog
 var express = require('express');
+var passport = require('passport');
+var Climber = require('../models/climber');
 var router = express.Router();
 
-/* GET users listing. */
+// KEEP AN EYE ON THIS ROUTE
 router.get('/', function (req, res) {
-  res.redirect('/climbs');
-    // res.render('catalog', { user : req.user });
+  res.render('index', { user: req.user });
+});
+
+router.get('/register', function (req, res) {
+  res.render('register', {});
+});
+
+router.post('/register', function (req, res) {
+  Climber.register(new Climber({
+    username: req.body.username
+  }), req.body.password, function (err, climber) {
+    if (err) {
+      return res.render('register', { climber: climber });
+    }
+
+    passport.authenticate('local')(req, res, function () {
+      res.redirect('/');
+    });
+  });
+});
+
+router.get('/login', function (req, res) {
+  res.render('login', { user: req.user });
+});
+
+router.post('/login', passport.authenticate('local'), function (req, res) {
+  res.redirect('/');
+});
+
+router.get('/logout', function (req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+router.get('/ping', function (req, res) {
+  res.status(200).send("pong!");
 });
 
 module.exports = router;
